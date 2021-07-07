51cebb1d1d44fd2c6720c02ef951df8cf860c8e1
João Gabriel <joaogabriel241201@gmail.com>
2021-07-07 11:26:09 -0300

    feature: POST da postagem

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
+import { Tema } from '../model/Tema';
+import { User } from '../model/User';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-inicio',
@@ -9,8 +14,19 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class InicioComponent implements OnInit {
 
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+  idTema: number
+
+  user: User = new User()
+  idUser: number = environment.id
+
+  postagem: Postagem = new Postagem()
+  
   constructor(
-    private router: Router
+    private router: Router,
+    private postagemService: PostagemService,
+    private temaService: TemaService
   ) { }
 
   ngOnInit(): void {
@@ -19,6 +35,29 @@ export class InicioComponent implements OnInit {
       //alert("Sua sessão expirou, faça login novamente")
       this.router.navigate(["/entrar"])
     }
+
+    this.findAllTemas()
+  }
+
+  findAllTemas() {
+    this.temaService.getAllTema().subscribe((resp: Tema[])=> {
+      this.listaTemas = resp
+    })
+  }
+
+  publicar(){
+    this.tema.id = this.idTema
+
+    this.user.id = this.idUser
+
+    this.postagem.tema = this.tema
+    this.postagem.usuario = this.user
+
+    this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem)=> {
+      this.postagem = resp
+      alert("Postagem publicada com sucesso!")
+      this.postagem = new Postagem()
+    })
   }
 
 }
