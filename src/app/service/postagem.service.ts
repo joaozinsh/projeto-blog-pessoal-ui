ab5c0d67bb5f6002098bc8d7e20d89860a3d1b91
João Gabriel <joaogabriel241201@gmail.com>
2021-07-07 23:39:49 -0300

    feature: PUT postagem

--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
@@ -21,7 +21,23 @@ export class PostagemService {
     return this.http.get<Postagem[]>("https://apiblogpessoaljg.herokuapp.com/postagens/buscar-todos", this.token)
   }
 
+  getByIdPostagem(id: number): Observable<Postagem> {
+    let params = new HttpParams()
+      .set('id', id)
+    return this.http.get<Postagem>(`https://apiblogpessoaljg.herokuapp.com/postagens/buscar-id?${params}`, this.token)
+  }
+
   postPostagem(postagem: Postagem): Observable<Postagem> {
     return this.http.post<Postagem>("https://apiblogpessoaljg.herokuapp.com/postagens/cadastrar", postagem, this.token)
+  }
+
+  putPostagem(postagem: Postagem): Observable<Postagem> {
+    return this.http.put<Postagem>("https://apiblogpessoaljg.herokuapp.com/postagens/alterar", postagem, this.token)
+  }
+
+  deletePostagem(id: number) {
+    let params = new HttpParams()
+      .set('id', id)
+    return this.http.delete(`https://apiblogpessoaljg.herokuapp.com/postagens/deletar?${params}`, this.token)
   }
 }
