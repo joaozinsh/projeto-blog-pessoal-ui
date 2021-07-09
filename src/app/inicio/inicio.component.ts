7dded47f1791febd2a047ad34765811516bd4ce6
João Gabriel <joaogabriel241201@gmail.com>
2021-07-09 19:50:06 -0300

    melhorias gerais

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -33,7 +33,8 @@ export class InicioComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-
+    window.scroll(0,0)
+    
     if(environment.token == "") {
       //alert("Sua sessão expirou, faça login novamente")
       this.router.navigate(["/entrar"])
